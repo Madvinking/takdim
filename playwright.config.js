f99fdc429bb872dc54afa0522a6139cf6a2931ac
nir winkler <madvinking@gmail.com>
2023-03-19 00:03:10 +0200

    allright

---
+++ playwright.config.js
@@ -0,0 +1,44 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+
+export default defineConfig({
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 120 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 1 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI ? 'line' : 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    headless: process.env.CI ? true : false,
+    actionTimeout: 0,
+    trace: 'off',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'], headless: process.env.CI ? true : false,
+      },
+    },
+  ],
+
+});
+
---
+++ tests/doctor.spec.js
@@ -0,0 +1,46 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import dayjs from 'dayjs';
+import twilio from 'twilio';
+
+const accountSid = process.env.TTTID;
+const authToken = process.env.TTTTOKEN;
+
+const client = twilio(accountSid, authToken);
+
+
+test('get docktor', async ({ page }) => {
+  await page.goto(process.env.LOGIN_URL);
+
+  await page.locator(`a[href="#IdentifyWithPassword"]`).click();
+  await page.locator(`#identifyWithPasswordCitizenId`).type(process.env.USERID);
+  await page.locator(`#password`).type(process.env.USERPASSWORD);
+  await new Promise(res => setTimeout(res, 1000));
+  await page.locator(`.validatePassword`).click();
+  await new Promise(res => setTimeout(res, 5000));
+  await page.goto(process.env.DOCTOR_URL);
+
+  await new Promise(res => setTimeout(res, 5000));
+  await page.locator(`.showSearchOnlineAuthntication`).click();
+
+  const text = await page.locator(`[class*="TimeSelect__availableForDateTitleTimeSelect"]`).textContent();
+  const firstFreeDate = text.match(/[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]/)?.at(0)?.trim();
+
+  const currentDate = dayjs(firstFreeDate, "DD/MM/YYYY");
+
+  const now = dayjs();
+  const next2Weeks = now.add(2, 'week');
+  if (currentDate.isBefore(next2Weeks) && currentDate.isAfter(now)) {
+    console.log('The target date is in the next 2 weeks: ', firstFreeDate);
+    await client.messages.create({
+      from: process.env.TTTTPHONE,
+      to: process.env.TTTMYPHONE,
+      body: `found appoinment at ${firstFreeDate}`
+    });
+  } else {
+    console.log('The target date is not in the next 2 weeks: ', firstFreeDate);
+  }
+
+  await new Promise(res => setTimeout(res, 30000));
+
+});
